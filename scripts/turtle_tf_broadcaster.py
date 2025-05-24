3bcb6dfe652272df92c26b7da97ba0a888f78e51
HebaAlmohammad <hebamh@yandex.com>
2025-05-25 02:47:22 +0300

    Completed ROS TF turtlesim project with both turtles working

--- scripts/turtle_tf_broadcaster.py
+++ scripts/turtle_tf_broadcaster.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 import rospy
 import tf
+import math
 from tf.transformations import quaternion_from_euler
 from turtlesim.msg import Pose
 
-rospy.init_node('tf_turtle')
-turtlename = rospy.get_param('~turtle_tf_name')
-
-def handle_turtle_pose(msg):
+def handle_turtle_pose(msg, turtlename):
     br = tf.TransformBroadcaster()
+    # Бродкаст преобразования черепахи относительно мира
     br.sendTransform((msg.x, msg.y, 0),
-                     quaternion_from_euler(0, 0, msg.theta),
-                     rospy.Time.now(),
-                     turtlename,
-                     "world")
+                    quaternion_from_euler(0, 0, msg.theta),
+                    rospy.Time.now(),
+                    turtlename,
+                    "world")
+    
+    # Бродкаст преобразования морковки, которая вращается вокруг черепахи
+    t = rospy.Time.now().to_sec()
+    br.sendTransform((0.3 * math.cos(t), 0.3 * math.sin(t), 0),
+                    quaternion_from_euler(0, 0, 0),
+                    rospy.Time.now(),
+                    "carrot_" + turtlename,
+                    turtlename)
 
-rospy.Subscriber('input_pose', Pose, handle_turtle_pose)
-rospy.spin()
+if __name__ == '__main__':
+    rospy.init_node('turtle_tf_broadcaster')
+    turtlename = rospy.get_param('~turtle')
+    rospy.Subscriber('/%s/pose' % turtlename,
+                    Pose,
+                    handle_turtle_pose,
+                    turtlename)
+    rospy.spin()
+
--- scripts/turtle_tf_listener.py
+++ scripts/turtle_tf_listener.py
@@ -1,29 +1,64 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import rospy
-import math
 import tf
+import math
+from turtlesim.srv import Spawn
 from geometry_msgs.msg import Twist
-import turtlesim.srv
+
+def spawn_turtle():
+    rospy.wait_for_service('spawn')
+    try:
+        spawn_turtle = rospy.ServiceProxy('spawn', Spawn)
+        response = spawn_turtle(4.0, 2.0, 0.0, "turtle2")
+        rospy.loginfo("Вторая черепашка успешно создана: %s", response.name)
+        return True
+    except rospy.ServiceException as e:
+        rospy.logerr("Не удалось создать вторую черепашку: %s", e)
+        return False
 
 if __name__ == '__main__':
-    rospy.init_node('tf_turtle')
+    rospy.init_node('turtle_tf_listener')
+    
+    # Попробовать создать черепашку до 3 раз при необходимости
+    for i in range(3):
+        if spawn_turtle():
+            break
+        rospy.sleep(1)
+    else:
+        rospy.logerr("Не удалось создать черепашку после 3 попыток")
+        exit(1)
+    
+    # Публикатор команд движения для turtle2
+    turtle_vel = rospy.Publisher('turtle2/cmd_vel', Twist, queue_size=1)
+    
+    # Слушатель трансформаций tf
     listener = tf.TransformListener()
     
-    rospy.wait_for_service('spawn')
-    spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-    spawner(4, 2, 0, 'turtle2')
+    # Подождать, пока трансформации станут доступны
+    rospy.sleep(2)
+    listener.waitForTransform('/turtle2', '/carrot_turtle1', rospy.Time(), rospy.Duration(4.0))
     
-    turtle_vel = rospy.Publisher('turtle2/cmd_vel', Twist, queue_size=1)
-    rate = rospy.Rate(10.0)
-    msg = Twist()
-    
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
-            (trans, rot) = listener.lookupTransform('/turtle2', '/carrot', rospy.Time())
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            (trans, rot) = listener.lookupTransform('/turtle2', '/carrot_turtle1', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+            rospy.logwarn("Ошибка при получении трансформации: %s", e)
+            rate.sleep()
             continue
         
-        msg.linear.x = 0.5 * math.sqrt(trans[0]**2 + trans[1]**2)
-        msg.angular.z = 4 * math.atan2(trans[1], trans[0])
-        turtle_vel.publish(msg)
+        # Расчёт угловой и линейной скоростей
+        angular = 4 * math.atan2(trans[1], trans[0])
+        linear = 0.5 * math.sqrt(trans[0]**2 + trans[1]**2)
+        
+        # Отправка команды движения
+        cmd = Twist()
+        cmd.linear.x = linear
+        cmd.angular.z = angular
+        turtle_vel.publish(cmd)
+        
         rate.sleep()
+
+
+
+
